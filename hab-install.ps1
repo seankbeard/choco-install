53187d18247dd91ab7a40547dbca04a39a6a6285
Sean Beard <sean.beard@dimensiondata.com>
2018-11-02 08:43:28 +1300

    adding hab install script

---
+++ hab-install.ps1
@@ -0,0 +1,5 @@
+﻿## Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+## Install Habitat Apps
+choco install git 7zip googlechrome vscode habitat -y
